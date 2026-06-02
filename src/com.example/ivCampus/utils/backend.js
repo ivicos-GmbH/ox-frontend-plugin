f124fd771821cc53654f5ffd3fed28a3d19afb9b
Md Asaduzzaman Ripan <62987442+aripan@users.noreply.github.com>
2026-06-02 18:03:26 +0200

    Refactor profile update API call

--- src/com.example/ivCampus/utils/backend.js
+++ src/com.example/ivCampus/utils/backend.js
@@ -1,7 +1,6 @@
 /* eslint-disable license-header/header */
 
 import { API_CONFIG } from './constants'
-import ox from '$/ox'
 
 /**
  * Handle profile update by calling backend API
@@ -11,19 +10,10 @@ import ox from '$/ox'
  */
 export const handleProfileUpdate = async (email, iframe) => {
   try {
-    const url = new URL(API_CONFIG.profileUpdate)
-    url.searchParams.set('email', email)
-
-    const response = await fetch(url.toString(), {
-      headers: {
-        Authorization: `Bearer ${ox.session}`
-      }
-    })
-
+    const response = await fetch(`${API_CONFIG.profileUpdate}?email=${email}`)
     if (!response.ok) {
       throw new Error(`Profile update failed: HTTP ${response.status}`)
     }
-
     const data = await response.json()
     if (data.success) {
       const currentSrc = iframe.attr('src')
